74eef0dbb776a868506c2c3cbd97a46d17c236f8
André Lopes <andrelopes.code@gmail.com>
2024-06-23 15:21:51 -0300

    🐛 fix: user repository inheritance

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -3,7 +3,7 @@ from app.models.user import User
 from .base_repository import AsyncCRUDRepositoryWithEmail, AsyncSession
 
 
-class UserRepository(AsyncCRUDRepositoryWithEmail([User])):
+class UserRepository(AsyncCRUDRepositoryWithEmail[User]):
     """User repository"""
 
     def __init__(self, session: AsyncSession):
