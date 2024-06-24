b46dacb26af406f59899a504201f187aea3f265c
André Lopes <andrelopes.code@gmail.com>
2024-06-24 15:41:50 -0300

    ✨ feat: add user service logic

---
+++ app/services/user_service.py
@@ -0,0 +1,56 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core import exc
+from app.models.user import User
+from app.repositories.user_repository import UserRepository
+from app.schemas.user_schemas import UserIn, UserUpdate
+
+
+class UserService:
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
+        self.repository = UserRepository(session)
+
+    async def create_user(self, data: UserIn) -> User:
+        user = User(**data.model_dump())
+
+        # Check if email is already in use by another user
+        if await self.email_in_use(user.email):
+            raise exc.BadRequestException('Email already in use')
+
+        created_user = await self.repository.save(user)
+        return created_user
+
+    async def update_user(
+        self,
+        user_id: int,
+        current_user: User,
+        data: UserUpdate,
+    ) -> User:
+        if user_id != current_user.id:
+            raise exc.UnauthorizedException('You cannot update this user')
+
+        updated_user = await self.repository.update(current_user, data)
+        if not updated_user:
+            raise exc.NotFoundException('User not found')
+
+        return updated_user
+
+    async def delete_user(self, user_id: int, current_user: User) -> User:
+        if user_id != current_user.id:
+            raise exc.UnauthorizedException('You cannot delete this user')
+
+        deleted_user = await self.repository.delete_by_id(user_id)
+        return deleted_user
+
+    async def email_in_use(self, email: str) -> bool:
+        """Check if an email is already in use
+
+        Args:
+            email (str): The email to be checked
+
+        Returns:
+            bool: True if email is in use, False otherwise
+        """
+        user = await self.repository.get_by_email(email)
+        return True if user else False
