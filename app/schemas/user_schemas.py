d37453a1d4e604a41de51de823ac2bfa1669a02a
André Lopes <andrelopes.code@gmail.com>
2024-06-24 15:41:19 -0300

    ✨ feat: add pydantic user schemas

---
+++ app/schemas/user_schemas.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from typing import Annotated
+
+from pydantic import BaseModel, EmailStr, Field
+
+from .custom_types import Password
+
+
+class UserFields:
+    name = Annotated[str, Field(max_length=255)]
+    email = Annotated[EmailStr, ...]
+    password = Annotated[
+        Password,
+        Field(
+            description='Must contain at least one number and one uppercase'
+            'and lowercase letter, and at least 8 or more characters',
+        ),
+    ]
+
+
+class UserIn(BaseModel):
+    name: UserFields.name
+    email: UserFields.email
+    password: UserFields.password
+
+
+class UserOut(BaseModel):
+    name: UserFields.name
+    email: UserFields.email
+    created_at: datetime
+    updated_at: datetime
+
+
+class UserUpdate(BaseModel):
+    name: UserFields.name
