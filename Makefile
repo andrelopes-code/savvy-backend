74423301c8e853520a117f6beae5a365cc833f98
André Lopes <andrelopes.code@gmail.com>
2024-06-24 21:51:08 -0300

    ✨ feat: create manual event_loop fixture

--- Makefile
+++ Makefile
@@ -4,4 +4,7 @@ run:
 	@fastapi dev --reload
 
 test:
-	@SAVVY_DATABASE_NAME=savvy_test pytest -s -x -v
+	@SAVVY_DATABASE_NAME=savvy_test pytest -svx
+
+cov:
+	@SAVVY_DATABASE_NAME=savvy_test pytest --cov && coverage html
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+import asyncio
+
+import pytest
 import pytest_asyncio
 from httpx import ASGITransport, AsyncClient
 from sqlalchemy import NullPool, text
@@ -9,6 +12,14 @@ from app.core.db.postgres import (
     get_db,
 )
 from app.main import app
+
+
+@pytest.fixture(scope='session')
+def event_loop():
+    """Create an instance of the default event loop for each test case."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
 
 
 @pytest_asyncio.fixture(scope='session')
