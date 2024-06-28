0c8743805a931bf1d0a072215b05b49bf98b4ebe
André Lopes <andrelopes.code@gmail.com>
2024-06-28 00:39:09 -0300

    ✨ feat: add record router

--- app/routers/v1/__init__.py
+++ app/routers/v1/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from .auth_router import router as auth_router
+from .record_router import router as record_router
 from .user_router import router as user_router
 
 # The main router that will be used by the app for all v1 routes
@@ -9,3 +10,4 @@ main_router = APIRouter(prefix='/api/v1')
 # Include sub-routers
 main_router.include_router(auth_router, tags=['Auth'])
 main_router.include_router(user_router, tags=['User'])
+main_router.include_router(record_router, tags=['Record'])
---
+++ app/routers/v1/record_router.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter
+
+from app.core.dependencies import (
+    AsyncDBSessionDepends,
+    AuthenticatedDBUserDepends,
+)
+from app.schemas.record_schemas import (
+    RecordIn,
+    RecordOut,
+    RecordWithCategoryOut,
+)
+from app.services.record_service import RecordService
+
+router = APIRouter()
+
+
+@router.get('/records', response_model=list[RecordWithCategoryOut])
+async def get_user_records(
+    user: AuthenticatedDBUserDepends,
+    session: AsyncDBSessionDepends,
+    sort: str | None = None,
+):
+    record_service = RecordService(session, user)
+    return await record_service.get_user_records(sort=sort)
+
+
+@router.post('/records', response_model=RecordWithCategoryOut)
+async def create_record(
+    user: AuthenticatedDBUserDepends,
+    session: AsyncDBSessionDepends,
+    data: RecordIn,
+):
+    record_service = RecordService(session, user)
+    return await record_service.create_record(data)
+
+
+@router.delete('/records', response_model=RecordOut)
+async def delete_record(
+    user: AuthenticatedDBUserDepends,
+    session: AsyncDBSessionDepends,
+    record_id: int,
+):
+    record_service = RecordService(session, user)
+    return await record_service.delete_record(record_id)
