d5b00a6240e732b64826eec0b371e687f6d40abc
André Lopes <andrelopes.code@gmail.com>
2024-06-28 00:37:27 -0300

    ✨ feat: fix timestamp without timezone in record model

--- app/models/record.py
+++ app/models/record.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import ForeignKey, Integer
+from sqlalchemy import DateTime, ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
@@ -13,7 +13,7 @@ class Record(Base):
     amount: Mapped[int] = mapped_column(nullable=False, index=True)
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
     description: Mapped[str] = mapped_column(nullable=False)
-    date: Mapped[datetime]
+    date: Mapped[datetime] = mapped_column(DateTime(True), nullable=False)
     category_id: Mapped[int] = mapped_column(ForeignKey('categories.id'))
 
     category = relationship('Category')
