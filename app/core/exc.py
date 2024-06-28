f5956514e4bea2ef160bc4455b7c9ecd1c0ea4a8
André Lopes <andrelopes.code@gmail.com>
2024-06-28 15:06:23 -0300

    ✨ feat: add ForbiddenException into exc module

--- app/core/exc.py
+++ app/core/exc.py
@@ -48,3 +48,8 @@ class InternalServerErrorException(HTTPException):
         super().__init__(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=detail
         )
+
+
+class ForbiddenException(HTTPException):
+    def __init__(self, detail: str = 'Forbidden'):
+        super().__init__(status_code=status.HTTP_403_FORBIDDEN, detail=detail)
