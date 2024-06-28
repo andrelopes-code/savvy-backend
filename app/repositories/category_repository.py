77e2130fe40c5b31274663926e2e84ecf02fedc4
André Lopes <andrelopes.code@gmail.com>
2024-06-28 15:06:44 -0300

    ✨ feat: add delete_instance method to CategoryRepository

--- app/repositories/category_repository.py
+++ app/repositories/category_repository.py
@@ -6,3 +6,7 @@ from .base_repository import AsyncCRUDRepository
 class CategoryRepository(AsyncCRUDRepository[Category]):
     def __init__(self, session):
         super().__init__(session, Category)
+
+    async def delete_instance(self, instance: Category) -> None:
+        await self.session.delete(instance)
+        await self.session.commit()
