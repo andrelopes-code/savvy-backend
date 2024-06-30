440f4c84d6e15d1f38fedc9453467858b1e7c87b
André Lopes <andrelopes.code@gmail.com>
2024-06-30 00:03:13 -0300

    ✅ test: add more test to security depends

--- tests/v1/test_security/test_security_dependencies.py
+++ tests/v1/test_security/test_security_dependencies.py
@@ -1,7 +1,9 @@
 import pytest
 from httpx import AsyncClient
 
+from app.core import exc
 from app.core.db.postgres import AsyncSession, get_db
+from app.core.sec import get_current_user, get_db_user
 
 
 @pytest.mark.asyncio()
@@ -15,3 +17,14 @@ async def test_not_authenticated_user_dependency(client: AsyncClient):
 async def test_get_db_dependency():
     async for s in get_db():
         assert isinstance(s, AsyncSession)
+
+
+@pytest.mark.asyncio()
+async def test_get_db_user_dependency():
+    with pytest.raises(exc.UnauthorizedException):
+        await get_db_user({'sub': 123})
+
+
+def test_get_current_user_dependency_invalid_token_type():
+    with pytest.raises(exc.UnauthorizedException):
+        get_current_user('Invalid token')
